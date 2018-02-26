4aff8288a6c93cdb53dd103ec8df87913b309f96
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2018-02-26 16:23:30 -0500

    manejador de errores

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const menusRoutes = require('./api/routes/menus');
 const  pacientsRoutes = require('./api/routes/pacients');
 
+app.use(morgan('dev'));
+
+//Rutas  que se encargan de los requests
 app.use('/menus', menusRoutes);
 app.use('/pacients',pacientsRoutes);
 
+
+app.use(function(req,res,next){
+   const error = new Error('Not found');
+   error.status(404);
+   next(error);
+});
+
+
+app.use(function(error, req, res, next){
+    res.status(err.status|| 500);
+    res.json({
+        message:error.message
+    });
+});
 
 
 module.exports = app;
