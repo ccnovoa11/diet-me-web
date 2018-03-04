47c7ba36600faabcb81fe73c471cdc168cce2785
Juan Diego <jd.gonzaleza@uniandes.edu.co>
2018-03-04 12:07:48 -0500

    Cambios y findDay

--- front-end/src/components/UserInfo/PersonalInfo.js
+++ front-end/src/components/UserInfo/PersonalInfo.js
@@ -1,25 +1,29 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-export class PersonalInfo extends Component{
+export class PersonalInfo extends Component {
 
-	render(){
-		return(
-			<div className="container">
-				<div className="row">
-					<div className="col-sm-3">
-						<p>Name: {this.props.user.name}</p>
-					</div>
-					<div className="col-sm-3">
-						<p>Age: {this.props.user.age}</p>
-					</div>
-					<div className="col-sm-3">
-						<p>Sex: {this.props.user.sex}</p>
-					</div>
-					<div className="col-sm-3">
-						<p>PhoneNumber: {this.props.user.phoneNumber}</p>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <label>Name</label>
+            <input className="textInfo" type="text" name="name" value={this.props.user.name}/>
+          </div>
+          <div className="col-sm-6">
+            <label>Age</label>
+            <input className="textInfo" type="text" name="age" value={this.props.user.age}/>
+          </div>
+          <div className="col-sm-6">
+            <label>Sex</label>
+            <input className="textInfo" type="text" name="sex" value={this.props.user.sex}/>
+          </div>
+          <div className="col-sm-6">
+            <label>PhoneNumber</label>
+            <input className="textInfo" type="text" name="p-number" value={this.props.user.phoneNumber}/>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- front-end/src/components/UserInfo/UserInfo.js
+++ front-end/src/components/UserInfo/UserInfo.js
@@ -1,40 +1,42 @@
-import React, {Component} from "react";
-import {DietInfo} from "./DietInfo";
-import {PersonalInfo} from "./PersonalInfo";
+import React, { Component } from "react";
+import { DietInfo } from "./DietInfo";
+import { PersonalInfo } from "./PersonalInfo";
 
-export class UserInfo extends Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			pacient:{}
-		};
-	}
-	
-	getPacient(){
-		fetch("/pacients/5a9b0bc7306f9c200c903ce0",{
-			headers:{
-				token:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imx1Y2hvN0BqdWFuLmNvbSIsInVzZXJpZCI6IjVhOWIwYmM3MzA2ZjljMjAwYzkwM2NkZiIsIm1lZGljIjpmYWxzZSwiaWF0IjoxNTIwMTM0MzEyLCJleHAiOjE1MjAxMzc5MTJ9.KaIhQqhc6edYz7nOlO36ixixNEw2W7TDfNp_9nWQoFw"
-			}
-		})
-			.then(res => res.json())
-			.then( p => this.setState({ pacient: p.pacient}));
-	}
+export class UserInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pacient: {}
+    };
+  }
 
-	componentDidMount(){
-		this.getPacient();
-	}
+  getPacient() {
+    var token = localStorage.getItem("token");
+    var idPac = localStorage.getItem("idPacient");
+    fetch("/pacients/"+idPac, {
+      headers: {
+        token: token 
+      }
+    })
+      .then(res => res.json())
+      .then(p => this.setState({ pacient: p.pacient }));
+  }
 
-	render(){
-		console.log(this.state.pacient);
-		return(
-			<div>
-				<div>
-					<PersonalInfo user={this.state.pacient}/>
-				</div>
-				<div>
-					<DietInfo user={this.state.pacient}/>
-				</div>
-			</div>
-		);
-	}
+  componentDidMount() {
+    this.getPacient();
+  }
+
+  render() {
+    console.log(this.state.pacient);
+    return (
+      <div>
+        <div>
+          <PersonalInfo user={this.state.pacient} />
+        </div>
+        <div>
+          <DietInfo user={this.state.pacient} />
+        </div>
+      </div>
+    );
+  }
 }
